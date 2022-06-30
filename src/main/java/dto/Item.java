253d9b9fa918e614d6a906ae8f9d08fc0f95ca27
Steven Kwok <derekkwok2020@gmail.com>
2022-06-30 09:26:12 +0100

    Item Class
    created

---
+++ src/main/java/dto/Item.java
@@ -0,0 +1,86 @@
+package src.main.java.dto;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Item {
+
+    private String name;
+    private BigDecimal price;
+    private int inventory; // number of items in inventory
+
+    // constructors
+    public Item(String name, String price, int inventory) {
+        this.name = name;
+        this.price = new BigDecimal(price);
+        this.inventory = inventory;
+    }
+
+    // Methods for overriding
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode(this.name);
+        hash = 31 * hash + Objects.hashCode(this.price);
+        hash = 31 * hash + this.inventory;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        if (this.inventory != other.inventory) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.price, other.price)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "name=" + name + ", price=" + price + ", inventory=" + inventory + '}';
+    }
+
+
+
+    // getters and setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+}
--- src/main/java/dto/vendingMachineFactory.java
+++
@@ -1,4 +0,0 @@
-package src.main.java.dto;
-
-public class vendingMachineFactory {
-}
