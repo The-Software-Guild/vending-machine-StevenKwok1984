3baf399243580ea21ed738250c22af1fbe69d7be
Steven Kwok <derekkwok2020@gmail.com>
2022-06-30 13:58:42 +0100

    Service Layer
    completed

--- src/main/java/service/VendingMachineServiceLayer.java
+++ src/main/java/service/VendingMachineServiceLayer.java
@@ -1,4 +1,20 @@
 package src.main.java.service;
 
+import src.main.java.dao.VendingMachinePersistenceException;
+import src.main.java.dto.Item;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
 public interface VendingMachineServiceLayer {
+    void checkIfEnoughMoney(Item item, BigDecimal inputMoney) throws  InsufficientFundsException;
+
+    Map<String, BigDecimal> getItemsInStockWithPrices() throws VendingMachinePersistenceException;
+
+    Map<BigDecimal, BigDecimal> getChangePerCoin(Item item, BigDecimal money);
+
+    Item getItem(String name, BigDecimal inputMoney) throws
+                                                            InsufficientFundsException,
+                                                            NoItemInventoryException,
+                                                            VendingMachinePersistenceException;
 }
--- src/main/java/service/VendingMachineServiceLayerImpl.java
+++ src/main/java/service/VendingMachineServiceLayerImpl.java
@@ -1,4 +1,72 @@
 package src.main.java.service;
 
+import src.main.java.dao.VendingMachineAuditDao;
+import src.main.java.dao.VendingMachineDao;
+import src.main.java.dao.VendingMachinePersistenceException;
+import src.main.java.dto.Change;
+import src.main.java.dto.Item;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
 public class VendingMachineServiceLayerImpl implements VendingMachineServiceLayer{
+
+    private VendingMachineAuditDao auditDao;
+    private VendingMachineDao dao;
+
+    public VendingMachineServiceLayerImpl(VendingMachineAuditDao auditDao, VendingMachineDao dao){
+        this.auditDao = auditDao;
+        this.dao = dao;
+    }
+
+    @Override
+    public void checkIfEnoughMoney(Item item, BigDecimal inputMoney) throws  InsufficientFundsException {
+        if (item.getPrice().compareTo(inputMoney) == 1) {
+            throw new InsufficientFundsException(
+            "Error: insufficient funds, you have only input " + inputMoney);
+        }
+    }
+
+    @Override
+    public Map<String, BigDecimal> getItemsInStockWithPrices() throws VendingMachinePersistenceException{
+        Map<String, BigDecimal> itemsInStockWithPrices = dao.getMapOfItemNamesInStockWithPrices();
+        return itemsInStockWithPrices;
+    }
+
+    @Override
+    public Map<BigDecimal, BigDecimal> getChangePerCoin(Item item, BigDecimal money){
+        BigDecimal itemPrice = item.getPrice();
+        Map<BigDecimal, BigDecimal> changeDuePerCoin = Change.changeDuePerCoin(itemPrice, money);
+        return changeDuePerCoin;
+    }
+
+    @Override
+    public Item getItem(String name, BigDecimal inputMoney) throws
+                                                            InsufficientFundsException,
+                                                            NoItemInventoryException,
+                                                            VendingMachinePersistenceException {
+        Item wantedItem = dao.getItem(name);
+
+        if (wantedItem == null) {
+            throw new NoItemInventoryException(
+                    "Error: there are no " + name + "'s in the vending machine.");
+        }
+
+        checkIfEnoughMoney(wantedItem, inputMoney);
+
+        removeOneItemFromInventory(name);
+        return wantedItem;
+    }
+
+    private void removeOneItemFromInventory(String name) throws
+                                                        NoItemInventoryException,
+                                                        VendingMachinePersistenceException {
+        if (dao.getItemInventory(name) >0) {
+            dao.removeAnItemFromInventory(name);
+            auditDao.writeAuditEntry(" One " + name + " removed.");
+        } else {
+            throw new NoItemInventoryException("Error: " + name + " is out of stock.");
+        }
+    }
+
 }
